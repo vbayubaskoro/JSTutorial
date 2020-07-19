4c53ac31fda35675c7956a2ee7962950ac7a3cc2
vbayubaskoro <vbayubaskoro@gmail.com>
2020-07-18 17:36:56 -0700

    added prototype stuff

--- main.js
+++ main.js
@@ -147,13 +147,89 @@ const todoCompleted = todos.filter(function(todo){
 })
 
 // CONDITIONALS
-const v = '10';
-//use === to match type and value
+const v = 10;
+//use === to match type and value (triple equal)
 if(v === 10){
     console.log("v is 10");
-} else {
+} else if(x>10) {
+    console.log("v is greater 10");
+}
+else {
     console.log("v is not 10");
 }
+
+// TERNIRARY 
+const m = 11;
+//if m>10 is true then you get red
+// if m > 10 is false then you get blue
+const color = m > 10 ? 'red' : "blue";
+console.log(color);
+
+//SWITCHES: evaluate the condition
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red of blue');
+        break;
+}
+
+//FUNCTION 
+function addNums(num1,num2){
+    return num1 +num2;
+}
+console.log(addNums(1,2));
+// change function into a var const, no need to "return",  the arrow does it
+ const addFive = num1 =>  console.log(num1 + 5);
+ addFive(5)
+
+ // todos.forEach((todo) => console.log(todo))
+
+// OBJECT ORIENTED PROGRAMMING
+//construct objects from constructive function 
+// Constructor Function
+// can add methods: functions to this person object
+function Person(firstName,lastName, dob){
+    this.firstName = firstName; //properties of objects
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    /*this.getBirthYear = function(){ // tthis is a method
+        return this.dob.getFullYear;
+    }
+    this.getFullName = function(){
+        return `${this.firstName} ${this.lastName}`;
+    }*/
+
+}
+//Prototype: another obkect, want to put the methods into prrototype
+// methods: functions in the object
+// prototype is an object and we can attach property to prototype
+// not have functions with every object instance
+// do this instead 
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear()
+}
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`
+
+}
+
+//Instantiate object
+const person1 = new Person('Vanessa', 'Teresa', '3-1-1998');
+const person2 = new Person('Ashley', 'K', '5-8-1997');
+
+console.log(person2);
+
+// Methods which are just function to this person/objcet
+
+
+
+
+
 
 
 
